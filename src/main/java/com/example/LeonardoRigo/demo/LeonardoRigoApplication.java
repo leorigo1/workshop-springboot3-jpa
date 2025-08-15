4dafd602da3be25b107e217662c66a6fb57b7a25
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-08-15 13:16:31 -0300

    Created project

---
+++ src/main/java/com/example/LeonardoRigo/demo/LeonardoRigoApplication.java
@@ -0,0 +1,13 @@
+package com.example.LeonardoRigo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonardoRigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeonardoRigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LeonardoRigo/demo/LeonardoRigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LeonardoRigo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeonardoRigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
